e4814392a6d73f8620091784eae31a6acacc7e6b
dorythecat <pemegomachado@gmail.com>
2026-05-10 18:25:14 +0200

    Added options to turn off the graph display, and to save the graph Also optimized code structure a bit

--- main.py
+++ main.py
@@ -18,18 +18,23 @@ class MType(Enum):
     DIODE       = "DIOD"
     CONTINUITY  = "CONT"
 
-# Settings
+# Measurement Settings
 # TODO: Make these configurable from the command-line/UI
 PLF: int    = 50                # Frequency of the powerline, in Hertz
 NPLC: float = 10                # Number of powerline cycles between measurements
 N: int      = 100               # Number of measurements to take (Set to -1 for infinite)
 TYPE: MType = MType.DC_VOLTAGE  # Type of the measurement to take
 
-# Auto-generated values from settings above
+# Data settings
+REALTIME_GRAPH: bool = True        # Should we plot a real-time graph?
+SHOW_GRAPH: bool     = True        # Should we show a graph at the end?
+SAVE_GRAPH: bool     = True        # Should we save a graph?
+GRAPH_FILE: str      = "graph.png" # Name of the file to save the graph to
+
+# Stuff that needs to be generated from settings above
 DELAY_TIME = 1.5 * (NPLC / PLF) # Multiply by 1.5 to give a good margin
 
-xs = []
-ys = []
+measurements = []                # Store the measurements we're about to take
 mpl.rcParams['toolbar'] = 'None' # Disable graph toolbar
 plt.style.use('dark_background') # Don't burn my retinas
 with serial.Serial('/dev/ttyUSB0', 9600, timeout=3) as ser:
@@ -37,17 +42,22 @@ with serial.Serial('/dev/ttyUSB0', 9600, timeout=3) as ser:
     ser.write(f':SENS:FUNC "{TYPE.value}"\n'.encode()) # Set type of measurement to DC voltage
     ser.write(f':SENS:VOLT:DC:NPLC {NPLC}\n'.encode()) # Set measurements to the highest sampling rate
     ser.write(b':INIT:CONT ON\n') # Continuously sample data
-    time.sleep(DELAY_TIME)
+    time.sleep(DELAY_TIME) # Make sure the first measurement has had time to settle
 
     i = 0
     while i != N:
-        xs.append(i)
         ser.write(b':SENS:DATA?\n') # Take a measuremeny
-        y = float(ser.readline())
-        ys.append(y)
-        plt.plot(xs,ys,color='red')
+        measurements.append(float(ser.readline()))
         if N > 0:
             i += 1
-        plt.pause(DELAY_TIME) # Wait for the next measurement to be properly taken
+        plt.plot(measurements, color='red')
+        if REALTIME_GRAPH:
+            plt.pause(DELAY_TIME)
+        else:
+            time.sleep(DELAY_TIME)
 
-plt.show()
+if SAVE_GRAPH:
+    plt.savefig(GRAPH_FILE)
+
+if SHOW_GRAPH:
+    plt.plot()
