3d16b751172912311523efa0b40cacfffaa4cfbb
dorythecat <pemegomachado@gmail.com>
2026-05-12 10:36:57 +0200

    Added new endpoints and improved API

--- api.py
+++ api.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 import interfaces.dmm.dmm as dmm
 import interfaces.dmm.keithley as keithley
@@ -31,18 +32,57 @@ keithley2000 = keithley.Keithley2000()
 
 @app.get("/system/interfaces", tags=["System"])
 async def interfaces() -> dict:
+    """
+    Displays the available interfaces on this API.
+    """
     return {
             "dmm": [
-                keithley2000.id
+                "keithley2000"
             ]
     }
 
-@app.get("/dmm/keithley/measure", tags=["DMM"])
-async def keithley_measure() -> float:
+@app.get("/dmm/keithley2000", tags=["DMM"])
+async def keithley2000_id() -> str:
+    return keithley2000.id
+
+@app.get("/dmm/keithley2000/measure", tags=["DMM"])
+async def keithley2000_measure() -> float:
+    # TODO: Actually implement a proper measurement endpoint
     keithley2000.measure_set()
     return keithley2000.measure_get()
 
-@app.post("/dmm/keithley/text/display", tags=["DMM"])
-async def text_display(enable: bool = True, text: str = "") -> None:
+@app.get("/dmm/keithley2000/input", tags=["DMM"])
+async def keithley2000_input() -> bool:
+    return keithley2000.input
+
+@app.get("/dmm/keithley2000/beeper", tags=["DMM"])
+async def keithley2000_beeper() -> bool:
+    return keithley2000.beeper
+
+@app.post("/dmm/keithley2000/beeper", tags=["DMM"])
+async def keithley2000_beeper(on: bool = False) -> None:
+    keithley2000.beeper = on
+
+class DisplaySettings(BaseModel):
+    enabled: bool
+    text: str
+
+@app.get("/dmm/keithley2000/display", tags=["DMM"])
+async def keithley2000_display() -> DisplaySettings:
+    return DisplaySettings(
+        enabled=keithley2000.display,
+        text=keithley2000.text
+    )
+
+@app.post("/dmm/keithley2000/display", tags=["DMM"])
+async def keithley2000_display(enable: bool = True, text: str = "") -> None:
     keithley2000.display = enable
     keithley2000.text = text
+
+@app.get("/dmm/keithley2000/autozero", tags=["DMM"])
+async def keithley2000_autozero() -> bool:
+    return keithley2000.autozero
+
+@app.post("/dmm/keihtley2000/autozero", tags=["DMM"])
+async def keithley2000_autozero(on: bool = True) -> None:
+    keithley2000.autozero = on
