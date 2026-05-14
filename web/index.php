2825785ef67110b3f22afdd6a3f3f8ec0d48bc93
dorythecat <pemegomachado@gmail.com>
2026-05-14 13:37:06 +0200

    Improved the page structure and added CSS

--- web/index.php
+++ web/index.php
@@ -5,22 +5,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>Connect-a-tron</title>
+
+  <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
 
+<div id="page-title">
+  <h1>CONNECT-A-TRON</h1>
+</div>
+
+<h2>Keithley 2000</h1>
 <?php
 const BASE_URL = 'http://127.0.0.1:8000/dmm/keithley2000';
-
-$ch = curl_init();
-curl_setopt_array($ch, [
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_URL => BASE_URL
-]);
-$name = curl_exec($ch);
-curl_close($ch);
-echo "<h1>$name</h1>";
-
   if(isset($_POST['measure'])) {
     $ch = curl_init();
     curl_setopt_array($ch, [
