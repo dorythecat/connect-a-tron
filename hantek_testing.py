5374597241a227e4a86d0361795263492b377263
dorythecat <pemegomachado@gmail.com>
2026-05-26 20:23:32 +0200

    Added a few useful values to quickly read from the oscilloscope

--- hantek_testing.py
+++ hantek_testing.py
@@ -4,6 +4,10 @@ import backend.interfaces.oscilloscope.hantek as hantek
 
 osc = hantek.DSO2D15()
 
+print(f"Frequency: {osc.frequency()} Hz")
+print(f"Period: {osc.period()} s")
+print(f"RMS: {osc.rms()} V")
+print(f"PPK: {osc.ppk()} V")
 ch1 = osc.get_waveform(mode="HRES")
 plt.plot(ch1, 'b')
 plt.show()
