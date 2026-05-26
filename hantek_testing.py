4f788243f187cabd4d0e5a9545aaa0dd07d2a1a8
dorythecat <pemegomachado@gmail.com>
2026-05-26 22:20:47 +0200

    Added arbitrary waveform generator control to Hantek DSO2000 series Also added an ad-hoc frequency response demonstration

--- hantek_testing.py
+++ hantek_testing.py
@@ -8,6 +8,14 @@ print(f"Frequency: {osc.frequency()} Hz")
 print(f"Period: {osc.period()} s")
 print(f"RMS: {osc.rms()} V")
 print(f"PPK: {osc.ppk()} V")
-ch1 = osc.get_waveform(mode="HRES")
-plt.plot(ch1, 'b')
+#ch1 = osc.get_waveform(mode="HRES")
+#plt.plot(ch1, 'b')
+#plt.show()
+
+freq = range(10000, 1000000, 10000)
+resp = []
+for i in freq:
+    osc.set_waveform(freq=i, amp=5)
+    resp.append(osc.ppk())
+plt.plot(freq, resp)
 plt.show()
