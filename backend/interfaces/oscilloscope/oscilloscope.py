9649d9a1af07371b5212a3b73f97295d8faac602
dorythecat <pemegomachado@gmail.com>
2026-05-30 23:58:03 +0200

    Reverted dumb change

--- backend/interfaces/oscilloscope/oscilloscope.py
+++ backend/interfaces/oscilloscope/oscilloscope.py
@@ -2,7 +2,7 @@ class Oscilloscope:
     def __init__(self, port: str) -> None:
         self._port: str = port
 
-        self._conn = open(port, "r+b", buffering=0, newline='\n') # We open the serial communication as a file
+        self._conn = open(port, "r+b", buffering=0) # We open the serial communication as a file
 
     def __del__(self) -> None:
         self._conn.close() # Close the file properly once we're done
