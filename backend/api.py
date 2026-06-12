29e6f8036baee39ab2315e52319aab8649efc790
dorythecat <pemegomachado@gmail.com>
2026-06-12 17:39:35 +0200

    now for sure!

--- backend/api.py
+++ backend/api.py
@@ -48,8 +48,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://0.0.0.0",
-        "http://0.0.0.0:8000"
+        "http://127.0.-1.1:8000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
