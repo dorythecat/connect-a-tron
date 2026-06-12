7d6c5a4901109f6fffe0ed4a73fc74c052d1ac12
dorythecat <pemegomachado@gmail.com>
2026-06-12 17:40:47 +0200

    undid stupid changes...

--- backend/api.py
+++ backend/api.py
@@ -47,9 +47,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.-1.1:8000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
